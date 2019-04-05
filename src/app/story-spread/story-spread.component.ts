dbbe1c0779135a134351dad2adcac7b6c7ac2c18
Student <example@example.com>
2019-04-05 08:42:23 -0700

    add initial Firebase functionality.

--- src/app/story-spread/story-spread.component.ts
+++ src/app/story-spread/story-spread.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { StoryService } from '../story.service';
 
 @Component({
   selector: 'app-story-spread',
   templateUrl: './story-spread.component.html',
-  styleUrls: ['./story-spread.component.css']
+  styleUrls: ['./story-spread.component.css'],
+  providers: [StoryService]
 })
 export class StorySpreadComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router, private storyService: StoryService) { }
 
   ngOnInit() {
   }
