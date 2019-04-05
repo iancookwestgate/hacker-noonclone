073b071ea877340c8bf43743af21905a99865286
Student <example@example.com>
2019-04-05 08:32:49 -0700

    add new service file for stories.

---
+++ src/app/story.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class StoryService {
+  constructor() { }
+
+}
